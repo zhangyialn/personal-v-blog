8b6f4666ee9bbcb90eff9c6855d1c87dc88fd1d8
zyl <1282432710@qq.com>
2023-09-25 13:06:20 +0800

    update style

--- front-end/src/components/Navigator/Navigator.js
+++ front-end/src/components/Navigator/Navigator.js
@@ -1,47 +1,56 @@
-import React, {useRef} from 'react';
-import classes from './Navigator.module.css'
+import React from 'react';
+import  './Navigator.css'
 import {Link, NavLink} from "react-router-dom";
 const Navigator = () => {
 
+
+    let styleChanger = ()=>{
+        let change = document.querySelector('.change')
+        let bar = document.querySelector('.bar')
+        if(change.classList[1] ==='icon-menu2'){
+            change.classList.replace('icon-menu2','icon-off-search')
+            bar.classList.replace('navigator','newBar')
+        } else {
+            change.classList.replace('icon-off-search','icon-menu2')
+        }
+    }
+
     return (
         <div>
-            <div className={classes.navigator}>
+            <div className="navigator bar">
                 <h3>暮岁首阳</h3>
-                <div className={classes.right}>
-                    <div className={classes.menu}>
-                        <i className="iconfont icon-menu2"></i>
+                <div className="right">
+                  <div className="menu" onClick={styleChanger}>
+                      <i className="iconfont icon-menu2 change"></i>
                     </div>
-                    <div className={classes.delete} >
-                        <i className="iconfont icon-off-search" ></i>
+                    <div className="search">
+                      <i className="iconfont icon-nav-search"></i>
                     </div>
-                    <div className={classes.search}>
-                        <i className="iconfont icon-nav-search"></i>
-                    </div>
-                    <div className={classes.rightInner} >
+                    <div className="rightInner" >
                         <Link to="/about">
                             <i className="iconfont icon-timerauto"></i>
                             <p>关于</p>
                         </Link>
                     </div>
-                    <div className={classes.rightInner}>
+                    <div className="rightInner">
                         <Link exact to='/tags'>
                             <i className="iconfont icon-label_fill"></i>
                             <p>标签</p>
                         </Link>
                     </div>
-                    <div className={classes.rightInner}>
+                    <div className="rightInner">
                         <Link exact to='/categories'>
                             <i className="iconfont icon-menu"></i>
                             <p>分类</p>
                         </Link>
                     </div>
-                    <div className={classes.rightInner}>
+                    <div className="rightInner">
                         <Link exact to='/archives'>
                             <i className="iconfont icon-paper"></i>
                             <p>归档</p>
                         </Link>
                     </div>
-                    <div className={classes.rightInner}>
+                    <div className="rightInner">
                         <NavLink exact to="/"  >
                             <i className="iconfont icon-home2"></i>
                             <p>首页</p>
