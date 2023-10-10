0393f1aec15f3aa8b18d72e7fd6ce1ead90b313f
zhangyialn <1282432710@qq.com>
2023-10-10 19:53:38 +0800

    菜狗

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,46 @@
+import fs from 'node:fs'
+import  express  from 'express'
+import cors from 'cors'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-console.log(fileURLToPath(''));
+
+
+
+const app = express()
+app.use(cors())
+app.listen(3001,() => {
+    console.log('服务器启动了');
+})
+let p = fileURLToPath(import.meta.url)
+
+
+let a = path.resolve('/home/zyl/markdown/npm.md')
+
+
+
+app.get('/',(req,res) =>{
+    fs.readdir('/home/zyl/markdown', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const arr = []
+            data.toString().split(',').forEach((element) => {
+                fs.readFile(`/home/zyl/markdown/${element}`, (err, content) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        arr.push(content.toString())
+                    }
+                })
+            })
+        res.send(arr)
+        }
+    })
+})
+
+
+
 
 
 
