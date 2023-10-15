441f8a82c911c769707f032a3111497345917aed
zhangyialn <1282432710@qq.com>
2023-10-15 11:09:18 +0800

    promise

--- backend/index.js
+++ backend/index.js
@@ -16,27 +16,16 @@ let p = fileURLToPath(import.meta.url)
 
 
 app.get('/',(req,res) => {
-    fs.readdir('/home/zyl/markdown',(err,data) => {
-        if(err){
-            console.log(err);
-        } else {
-            let articleArr = []
-            let arr = data.toString().split(',') 
-            let length = arr.length
-            for(let i = 0;i < length;i++){
-                fs.readFile(`/home/zyl/markdown/${arr[i]}`,(err,content) => {
-                    if(err){
-                        console.log(err);
-                    } else {
-                        articleArr.push(content.toString())
-                    }
-                    if(articleArr.length === length) {
-                        res.send(articleArr)
-                    }
-                })
-            }
-        }
+    fsPromises.readdir('/home/zyl/markdown',{encoding: 'utf-8'})
+    .then(result => {
+        console.log(result);
+        const filePaths = result.map(ele => path.join('/home/zyl/markdown/',ele))
+        return Promise.all(filePaths.map(filePaths => fsPromises.readFile(filePaths,{encoding: 'utf-8'})))
     })
+    .then(result => {
+        res.send(result)
+    })
+    .catch(err => console.log(err))
 })
 
 
