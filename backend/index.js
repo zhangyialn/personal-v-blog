7052149b7f5177ad73e9f5e8648da53acf070d2d
zhangyialn <1282432710@qq.com>
2023-10-15 14:17:16 +0800

    开摆

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,33 @@
 import fsPromises from 'node:fs/promises'
-import  express  from 'express'
+import express from 'express'
 import cors from 'cors'
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-
-
 
 const app = express()
 app.use(cors())
-app.listen(3001,() => {
+app.listen(3001, () => {
     console.log('服务器启动了');
 })
 
-app.get('/',(req,res) => {
-    fsPromises.readdir('/home/zyl/markdown',{encoding: 'utf-8'})
-    .then(result => {
-        console.log(result);
-        const filePaths = result.map(ele => path.join('/home/zyl/markdown/',ele))
-        return Promise.all(filePaths.map(filePaths => fsPromises.readFile(filePaths,{encoding: 'utf-8'})))
-    })
-    .then(result => {
-        res.send(result)
-    })
-    .catch(err => console.log(err))
+app.get('/', (req, res) => {
+    fsPromises.readdir('/home/zyl/markdown', { encoding: 'utf-8' })
+        .then(result => {
+            const filePaths = result.map(ele => path.join('/home/zyl/markdown/', ele));
+
+            const articlePromises = filePaths.map(filePath => {
+                const fileName = path.basename(filePath);
+                return fsPromises.readFile(filePath, { encoding: 'utf-8' })
+                    .then(content => {
+                        return { title: fileName, content };
+                    });
+            });
+
+            return Promise.all(articlePromises);
+        })
+        .then(contents => {
+            res.send(contents)
+        })
+        .catch(err => console.log(err))
 })
 
 
